843bb65761d99993c2b0a8dddc568f30c2053eeb
dangnhatminh-windows <dangnhatminh09032002@gmail.com>
2022-02-15 05:01:16 +0700

    client_1

---
+++ client/src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Box from "@mui/material/Box";
+
+export default function Rightbar() {
+  return (
+    <Box sx={{ display: { xs: "none", sm: "block" }, width: 1, mx: 2 }}>
+      This is right bar
+    </Box>
+  );
+}
